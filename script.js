ce552d7068ec8179b5d57347ebfbbfd3f548767f
getmypandit <getmypandit@gmail.com>
2024-11-25 18:50:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("animateButton").addEventListener("click", function () {
+  const zodiacCircle = document.getElementById("zodiac-circle");
+  // Start the animation
+  zodiacCircle.classList.toggle("rotate");
+});
