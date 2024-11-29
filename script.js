d2f1265e16a9864e2356be82e7449ef03ff49ce2
getmypandit <getmypandit@gmail.com>
2024-11-29 16:46:07 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("animateButton").addEventListener("click", function () {
-  const zodiacCircle = document.getElementById("zodiac-circle");
-  // Start the animation
-  zodiacCircle.classList.toggle("rotate");
-});
